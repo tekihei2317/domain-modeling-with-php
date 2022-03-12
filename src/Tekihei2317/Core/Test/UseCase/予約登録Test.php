7c3bb5a737bbd0817dd3efc9f541ebb281a3a86d
tekihei2317 <tekihei2317@gmail.com>
2022-03-12 18:01:05 +0900

    予約登録ユースケースに永続化処理を追加

--- src/Tekihei2317/Core/Test/UseCase/予約登録Test.php
+++ src/Tekihei2317/Core/Test/UseCase/予約登録Test.php
@@ -5,26 +5,63 @@ declare(strict_types=1);
 namespace Tekihei2317\Core\Test\UseCase;
 
 use PHPUnit\Framework\TestCase;
-use Tekihei2317\Core\Domain\Exception\InvalidOperationException;
 use Tekihei2317\Core\Domain\Exception\PreconditionException;
 use Tekihei2317\Core\Domain\Model\予約;
 use Tekihei2317\Core\Domain\Model\予約接種日;
 use Tekihei2317\Core\Domain\Model\接種券番号;
 use Tekihei2317\Core\Domain\Model\自治体番号;
 use Tekihei2317\Core\Domain\Model\接種者;
+use Tekihei2317\Core\Domain\Port\接種者Command;
 use Tekihei2317\Core\Domain\Port\接種者Query;
 use Tekihei2317\Core\Subdomain\Model\Date;
 use Tekihei2317\Core\UseCases\予約登録UseCase;
 
 class 予約登録Test extends TestCase
 {
-    // TODO: 正常系
-    private 予約 $reservation;
-
     protected function setUp(): void
     {
         $today = Date::createFromString('2022-01-01');
         $this->reservation = new 予約(予約接種日::createFromString('2022-01-08', $today));
+    }
+
+    /**
+     * @test
+     */
+    public function run_()
+    {
+        $command = new class implements 接種者Command
+        {
+            public ?接種者 $接種者 = null;
+            public function store(接種者 $接種者): void
+            {
+                $this->接種者 = $接種者;
+            }
+        };
+
+        $useCase = new 予約登録UseCase(
+            new class implements 接種者Query
+            {
+                public function findBy接種券番号And自治体番号(接種券番号 $接種券番号, 自治体番号 $自治体番号): ?接種者
+                {
+                    return new 接種者(1);
+                }
+            },
+            $command
+        );
+
+        $reservationDate = 予約接種日::createFromString('2022-01-08', Date::createFromString('2022-01-01'));
+        $useCase->run(
+            new 接種券番号('0123456789'),
+            new 自治体番号('012345'),
+            $reservationDate,
+        );
+
+        $expected = new 接種者(
+            1,
+            new 予約($reservationDate)
+        );
+
+        $this->assertEquals($expected, $command->接種者);
     }
 
     /**
@@ -41,33 +78,12 @@ class 予約登録Test extends TestCase
                 {
                     return null;
                 }
-            }
-        );
-
-        $useCase->run(
-            new 接種券番号('0123456789'),
-            new 自治体番号('012345'),
-            予約接種日::createFromString('2022-01-08', Date::createFromString('2022-01-01'))
-        );
-    }
-
-    /**
-     * @test
-     */
-    public function run_予約済みの場合はエラーになること()
-    {
-        $this->expectException(InvalidOperationException::class);
-
-        $useCase = new 予約登録UseCase(
-            new class implements 接種者Query
+            },
+            new class implements 接種者Command
             {
-                public function findBy接種券番号And自治体番号(接種券番号 $接種券番号, 自治体番号 $自治体番号): ?接種者
+                public function store(接種者 $接種者): void
                 {
-                    $today = Date::createFromString('2022-01-01');
-                    return new 接種者(
-                        id: 1,
-                        予約: new 予約(予約接種日::createFromString('2022-01-08', $today))
-                    );
+                    throw new \BadMethodCallException();
                 }
             }
         );
@@ -78,6 +94,4 @@ class 予約登録Test extends TestCase
             予約接種日::createFromString('2022-01-08', Date::createFromString('2022-01-01'))
         );
     }
-    // 接種済みの場合
-    // 指定した日付が範囲外の場合
 }
--- src/Tekihei2317/Core/UseCases/予約登録UseCase.php
+++ src/Tekihei2317/Core/UseCases/予約登録UseCase.php
@@ -4,17 +4,19 @@ declare(strict_types=1);
 
 namespace Tekihei2317\Core\UseCases;
 
+use Tekihei2317\Core\Domain\Model\予約;
 use Tekihei2317\Core\Domain\Model\予約接種日;
 use Tekihei2317\Core\Domain\Model\接種券番号;
 use Tekihei2317\Core\Domain\Model\自治体番号;
 use Tekihei2317\Core\Domain\Port\接種者Query;
+use Tekihei2317\Core\Domain\Port\接種者Command;
 use Tekihei2317\Core\Domain\Exception\PreconditionException;
-use Tekihei2317\Core\Domain\Model\予約;
 
 final class 予約登録UseCase
 {
     public function __construct(
-        private 接種者Query $query
+        private 接種者Query $query,
+        private 接種者Command $commmand,
     ) {
     }
 
@@ -27,10 +29,9 @@ final class 予約登録UseCase
             throw new PreconditionException('該当する接種者が存在しません');
         }
 
-        // 予約登録を行う
         $予約 = new 予約($予約接種日);
-        $接種者->予約登録($予約);
+        $接種者 = $接種者->予約登録($予約);
 
-        // 例外を拾って処理する?
+        $this->commmand->store($接種者);
     }
 }
